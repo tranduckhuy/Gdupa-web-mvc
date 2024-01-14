69e741786b8167a52b599b85fb92a58819431a76
NguyenXuanQuy <nguyenxuanquy147@gmail.com>
2024-01-14 08:52:30 +0700

    Update Views

--- WarehouseWebMVC/Controllers/CustomerController.cs
+++ WarehouseWebMVC/Controllers/CustomerController.cs
@@ -4,33 +4,29 @@ using WarehouseWebMVC.Models;
 
 namespace WarehouseWebMVC.Controllers
 {
-	public class CustomerController : Controller
-	{
-		private readonly ILogger<CustomerController> _logger;
+    public class CustomerController : Controller
+    {
+        private readonly ILogger<CustomerController> _logger;
 
-		public CustomerController(ILogger<CustomerController> logger)
-		{
-			_logger = logger;
-		}
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
 
-		public IActionResult Customer()
-		{
-			return View();
-		}
+        public IActionResult Customer()
+        {
+            return View();
+        }
 
-<<<<<<< Updated upstream:WarehouseWebMVC/Controllers/CustomerController.cs
-		public IActionResult CustomerInformation()
-=======
-		public IActionResult UserInformation()
->>>>>>> Stashed changes:WarehouseWebMVC/Controllers/UserController.cs
-		{
-			return View();
-		}
+        public IActionResult CustomerInformation()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
--- WarehouseWebMVC/Controllers/ProductController.cs
+++ WarehouseWebMVC/Controllers/ProductController.cs
@@ -6,136 +6,101 @@ using WarehouseWebMVC.Services;
 
 namespace WarehouseWebMVC.Controllers
 {
-<<<<<<< Updated upstream
-	public class ProductController : Controller
-	{
-		private readonly ILogger<ProductController> _logger;
-		private readonly IProductService _productService;
-
-		public ProductController(ILogger<ProductController> logger, IProductService productService)
-		{
-			_logger = logger;
-			_productService = productService;
-		}
-
-		[HttpGet]
-		public IActionResult Product()
-		{
-			return View();
-		}
-
-		[HttpGet]
-		public IActionResult AddProduct()
-		{
-			return View();
-		}
-
-		[HttpGet]
-		public IActionResult UpdateProduct()
-		{
-			return View();
-		}
-
-		[HttpGet]
-		public IActionResult ProductDetail()
-		{
-			return View();
-		}
-
-		[HttpPost]
-		public IActionResult AddProduct(ProductDTO productDTO)
-		{
-			if (ModelState.IsValid)
-			{
-				if (_productService.Add(productDTO) != null)
-				{
-					return View("Product");
-				} else
-				{
-					return View("AddProduct", new { Message = "Error", Data = productDTO });
-				}
-				
-			} else
-			{
-				return View("AddProduct", new { Message = "Error" } );
-			}
-		}
-
-		[HttpPut]
-		public IActionResult UpdateProduct(ProductDTO productDTO)
-		{
-			if (ModelState.IsValid)
-			{
-				if (_productService.Update(productDTO))
-				{
-					return View("Product");
-				}
-				else
-				{
-					return View("UpdateProduct", new { Message = "Error", Data = productDTO });
-				}
-
-			}
-			else
-			{
-				return View("UpdateProduct", new { Message = "Error" });
-			}
-		}
-
-		[HttpDelete]
-		public IActionResult DeleteProduct(long productId)
-		{
-			if (ModelState.IsValid)
-			{
-				if (_productService.Delete(productId))
-				{
-					return View("Product");
-				}
-				else
-				{
-					return View("Product", new { Message = "Error" });
-				}
-			}
-			else
-			{
-				return View("Product", new { Message = "Error" });
-			}
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
-=======
     public class ProductController : Controller
     {
         private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
+        [HttpGet]
         public IActionResult Product()
         {
             return View();
         }
 
-        public IActionResult ProductDetail()
-        {
-            return View();
-        }
-
+        [HttpGet]
         public IActionResult AddProduct()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult UpdateProduct()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_productService.Add(productDTO) != null)
+                {
+                    return View("Product");
+                }
+                else
+                {
+                    return View("AddProduct", new { Message = "Error", Data = productDTO });
+                }
+
+            }
+            else
+            {
+                return View("AddProduct", new { Message = "Error" });
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_productService.Update(productDTO))
+                {
+                    return View("Product");
+                }
+                else
+                {
+                    return View("UpdateProduct", new { Message = "Error", Data = productDTO });
+                }
+
+            }
+            else
+            {
+                return View("UpdateProduct", new { Message = "Error" });
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(long productId)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_productService.Delete(productId))
+                {
+                    return View("Product");
+                }
+                else
+                {
+                    return View("Product", new { Message = "Error" });
+                }
+            }
+            else
+            {
+                return View("Product", new { Message = "Error" });
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -144,5 +109,4 @@ namespace WarehouseWebMVC.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
->>>>>>> Stashed changes
 }
