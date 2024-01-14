004578a67bc2c757e85f207232dae1b9548f17ec
NguyenXuanQuy <nguyenxuanquy147@gmail.com>
2024-01-14 13:35:20 +0700

    Update Views

---
+++ WarehouseWebMVC/Controllers/SupplierController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WarehouseWebMVC.Models;
+
+namespace WarehouseWebMVC.Controllers
+{
+	public class SupplierController : Controller
+	{
+		private readonly ILogger<SupplierController> _logger;
+
+		public SupplierController(ILogger<SupplierController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult SupplierGrid()
+		{
+			return View();
+		}
+
+		public IActionResult SupplierList()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
