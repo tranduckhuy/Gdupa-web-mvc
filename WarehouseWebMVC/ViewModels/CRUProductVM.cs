91416015c5b6a5fc799dad7115a40e08073e269c
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-01-16 21:17:00 +0700

    Fix UpdateProduct View, add Product

---
+++ WarehouseWebMVC/ViewModels/CRUProductVM.cs
@@ -0,0 +1,13 @@
+﻿using WarehouseWebMVC.Models.Domain;
+using WarehouseWebMVC.Models.DTOs.ProductDTO;
+
+namespace WarehouseWebMVC.ViewModels
+{
+    public class CRUProductVM
+    {
+        public ICollection<Supplier> Suppliers { get; set; } = new List<Supplier>();
+        public ICollection<Category> Categorys { get; set; } = new List<Category>();
+        public ICollection<Brand> Brands { get; set; } = new List<Brand>();
+        public CURProductDTO Product { get; set; } = null!;
+    }
+}
