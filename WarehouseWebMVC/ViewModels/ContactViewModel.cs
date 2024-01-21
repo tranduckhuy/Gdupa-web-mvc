4407a45c736437af76c6bc99508d7f38e721a14c
FUQN77 <139064996+FUQN77@users.noreply.github.com>
2024-01-22 00:56:03 +0700

    Add Contact and Sub

---
+++ WarehouseWebMVC/ViewModels/ContactViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WarehouseWebMVC.ViewModels;
+
+public class ContactViewModel
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+   
+    public string Subject { get; set; } = string.Empty;
+
+    public string Message { get; set; } = string.Empty;
+}   
